061084f02e8e8201df47ce07b2319bdddf31cc00
Tiago Almeida <tiagomeloalmeida@ua.pt>
2024-01-31 18:35:44 +0000

    dtypes

--- compute_information.py
+++ compute_information.py
@@ -7,11 +7,19 @@ import json
 @click.argument("model_checkpoint")
 @click.option("--context-percentage", type=float, default=0)
 @click.option("--context-tokens", type=int, default=0)
+@click.option("--dtype", type=str, default="float32")
 def main(collection_file, 
          model_checkpoint, 
          context_percentage,
-         context_tokens):
+         context_tokens,
+         dtype):
 
+        _dtype_option = {"torch_dtype": dtype}
+        if _dtype_option=="int8":
+            _dtype_option = {"load_in_8bit": True}
+        elif _dtype_option=="int4":
+            _dtype_option = {"load_in_4bit": True}
+        
         def read_jsonl(file_path):
             def gen():
                 with open(file_path) as f:
@@ -20,11 +28,16 @@ def main(collection_file,
                         
             return gen
         
-        estimator = HFNIEstimator(model_checkpoint)
+        estimator = HFNIEstimator(model_checkpoint, 
+                                  cache_dir="hf_cache",
+                                  model_kwargs={
+                                      "device_map": "auto",
+                                      **_dtype_option
+                                  })
         
         _model_name = model_checkpoint.replace("/","_")
         
-        _out_name = f"{_model_name}_{collection_file[:-12]}_P{context_percentage}.jsonl"
+        _out_name = f"{_model_name}_{collection_file[:-12]}_P{context_percentage}_{dtype}.jsonl"
         
         with open(_out_name, "w") as f:
             for out in estimator.information_from_generator(read_jsonl(collection_file),
